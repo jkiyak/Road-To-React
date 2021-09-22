d26c7b7d91c579c70f977181cec3293598a5ab7c
John <jkiyak@gmail.com>
2021-09-22 16:18:56 -0500

    Fundamentals of React - Meet the React Componen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
-const title = 'React';
+const welcome = {
+greeting: 'Hey',
+title: 'React',
+
+};
 
 function App() {
 
@@ -8,7 +12,11 @@ function App() {
 
 return (
 <div>
-<h1>Hello World</h1>
+<h1>{welcome.greeting} {welcome.title}</h1>
+
+<label htmlFor="search">Search: </label>
+<input id="search" type="text" />
+
 </div>
 );
 }
