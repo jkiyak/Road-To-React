68ffe0a367e89180e86721d1f7169e664624abed
John <jkiyak@gmail.com>
2021-09-23 22:07:57 -0500

    refactor code

--- src/App.js
+++ src/App.js
@@ -19,44 +19,38 @@ const list = [
   },
 ];
 
-function App() {
-  return (
+const App = () => (
   <div>
-  <h1>My Hacker Stories</h1>
-  <Search />
-  <hr />
-  <List />
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+
+    <hr />
+
+    <List />
   </div>
-  );
-  }
+);
 
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
-function List() {
-  return (
+const List = () => (
   <ul>
-  {list.map(function (item) {
-  return (
-  <li key={item.objectID}>
-  <span>
-  <a href={item.url}>{item.title}</a>
-  </span>
-  <span>{item.author}</span>
-  <span>{item.num_comments}</span>
-  <span>{item.points}</span>
-  </li>
-  );
-  })}
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
   </ul>
-  );
-  }
+);
 
-
-  function Search () {
-    return (
-     <div>
-       <label htmlFor="search">Search:</label>
-       <input id="search" type="text" />
-     </div> 
-    )
-  }
 export default App;
