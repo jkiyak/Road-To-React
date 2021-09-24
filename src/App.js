fe44a8f7eec723ae1a99ca63fa4e9ca6b16256cc
John <jkiyak@gmail.com>
2021-09-24 12:08:03 -0500

    lifting state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 const App = () => {
-
   const stories = [
     {
       title: 'React',
@@ -21,68 +20,53 @@ const App = () => {
     },
   ];
 
-//A
-const handleSearch = (event) => {
-//C
-console.log(event.target.value);
-};
-
-return (
-  <div>
-    <h1>My Hacker Stories</h1>
-
-    <Search onSearch={handleSearch} />
-
-    <hr />
-
-  <List list={stories} />
-  </div>
-);
-};
-
-const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
-
-  const handleChange = (event) => {
-   setSearchTerm(event.target.value);
-   
-   
-   
-// B
-props.onSearch(event);
-};
-  return (
-  <div>
-  <label htmlFor="search">Search: </label>
-  <input id="search" type="text" onChange={handleChange} />
-
-  <p>
-    Searching for <strong>{searchTerm}</strong>
-  </p>
-
-
-  </div>
-  );
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
 
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
+
 const List = (props) => (
-<ul>
-{props.list.map((item) => (
-<Item key={item.objectID} item={item} />
-))}
-</ul>
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
 );
 
 const Item = (props) => (
   <li>
-  <span>
-  <a href={props.item.url}>{props.item.title}</a>
-  </span>
-  <span>{props.item.author}</span>
-  <span>{props.item.num_comments}</span>
-  <span>{props.item.points}</span>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
   </li>
-  );
+);
 
 export default App;
