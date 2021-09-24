a3dd7a398f9d516ef6939b2869c12f50ef902383
John <jkiyak@gmail.com>
2021-09-23 23:41:17 -0500

    putting in changes for handler functions section

--- src/App.js
+++ src/App.js
@@ -19,44 +19,46 @@ const list = [
   },
 ];
 
-function App() {
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+
+    <hr />
+
+    <List />
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+  }
+
+
+
   return (
   <div>
-  <h1>My Hacker Stories</h1>
-  <Search />
-  <hr />
-  <List />
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" onChange={handleChange} />
   </div>
   );
-  }
+  };
 
-
-function List() {
-  return (
+const List = () => (
   <ul>
-  {list.map(function (item) {
-  return (
-  <li key={item.objectID}>
-  <span>
-  <a href={item.url}>{item.title}</a>
-  </span>
-  <span>{item.author}</span>
-  <span>{item.num_comments}</span>
-  <span>{item.points}</span>
-  </li>
-  );
-  })}
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
   </ul>
-  );
-  }
+);
 
-
-  function Search () {
-    return (
-     <div>
-       <label htmlFor="search">Search:</label>
-       <input id="search" type="text" />
-     </div> 
-    )
-  }
 export default App;
